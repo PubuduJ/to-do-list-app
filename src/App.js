818a78f7f677028f9af27e6628905da19608d141
Pubudu Janith <pubudujanith123@gmail.com>
2023-03-13 10:03:34 +0530

    Implement add and deleteItem logics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <>
-      <h1>Hello React</h1>
-    </>
-  );
+    const [items, setItems] = useState([]);
+
+    function addItem(inputText) {
+        setItems(prevItems => {
+            return [...prevItems, inputText];
+        });
+    }
+
+    function deleteItem(itemId) {
+        setItems(prevItems => {
+            return prevItems.filter((item, index) => {
+                return index !== itemId;
+            });
+        });
+    }
+
+    return (
+        <>
+            <h1>Hello React</h1>
+        </>
+    );
 }
 
 export default App;
